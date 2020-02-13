7890ed3bab4b7d3d4e00d8ba243a32bbd955f7f6
eduahApps <eduah33@gmail.com>
2020-02-13 14:22:57 +0000

    Dusk Setup

---
+++ tests/DuskTestCase.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests;
+
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Laravel\Dusk\TestCase as BaseTestCase;
+
+abstract class DuskTestCase extends BaseTestCase
+{
+    use CreatesApplication;
+
+    /**
+     * Prepare for Dusk test execution.
+     *
+     * @beforeClass
+     * @return void
+     */
+    public static function prepare()
+    {
+        static::startChromeDriver();
+    }
+
+    /**
+     * Create the RemoteWebDriver instance.
+     *
+     * @return \Facebook\WebDriver\Remote\RemoteWebDriver
+     */
+    protected function driver()
+    {
+        $arguments = [
+            '--disable-gpu',
+            '--verbose',
+            '--window-size=1920,1080',
+            '--no-sandbox'
+        ];
+
+        if(getenv('APP_ENV') !== 'local'){
+            $arguments[] = '--headless';
+
+        }
+
+        $options = (new ChromeOptions)->addArguments($arguments);
+
+        return RemoteWebDriver::create(
+            'http://localhost:9515', DesiredCapabilities::chrome()->setCapability(
+                ChromeOptions::CAPABILITY, $options
+            )
+        );
+    }
+}
